814e11f584f0a5c38da04382e073553a4e5dac00
risheethg <53946729+risheethg@users.noreply.github.com>
2025-10-10 23:17:29 +0530

    WIP queue #2

--- app/core/messaging.py
+++ app/core/messaging.py
@@ -1,6 +1,7 @@
 import json
 from kafka import KafkaProducer
 from .config import settings
+from .logger import logger
 
 producer = KafkaProducer(
     bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
@@ -15,4 +16,4 @@ def send_customer_event(event_type: str, customer_data: dict):
     }
     producer.send(settings.KAFKA_CUSTOMER_TOPIC, value=message)
     producer.flush()
-    print(f"Sent event '{event_type}' for customer ID {customer_data.get('id')} to Kafka.")
+    logger.info(f"Sent event '{event_type}' for customer ID {customer_data.get('id')}, {customer_data.get('name')} to Kafka.")
